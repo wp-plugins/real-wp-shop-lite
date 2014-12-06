33e999cd49c32f779ec286aa5193e886d50e01d0
vk011 <vk011@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-06 14:22:17 +0000

    ygyigigiuiu
    git-svn-id: https://plugins.svn.wordpress.org/real-wp-shop-lite/trunk@1039422 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- real-wp-shop-lite.php
+++ real-wp-shop-lite.php
@@ -5,7 +5,7 @@ Plugin Name: Real WP Shop Lite
 Plugin URI: http://jultranet.com/wp/realwpshop
 Description: Lite ecommerce solution for WordPress
 Author: Vojislav Kovacevic
-Version: 1.0
+Version: 2.0.2
 Author URI: http://jultranet.com/wp/realwpshop
 */
 
